4dc2c8538f242f84bb33712d86a20f6ff30d3872
Alejandro de Frutos <adefrutoscasado@gmail.com>
2024-02-05 12:13:47 +0100

    First implementation of extendedrrule

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,119 @@
 
-export const returnArg = <T>(arg: T): T => {
-  return arg
+
+import { datetime, RRule, RRuleSet, rrulestr } from 'rrule'
+import moment = require('moment')
+
+/**
+ * Returns if two ranges of dates are in intersection
+ *
+ * @param {Moment} eventStart
+ * @param {Moment} eventEnd
+ * @param {Moment} startThresoldDate
+ * @param {Moment} endThresoldDate
+ * @return {boolean}
+ */
+const eventIntersectThresold = (
+  eventStart: moment.Moment,
+  eventEnd: moment.Moment,
+  startThresoldDate: moment.Moment,
+  endThresoldDate: moment.Moment
+) =>
+  (
+    (
+    // case when thresold contains entirely the event. Cases when event limits cut the thresold.
+      eventStart.isBetween(startThresoldDate, endThresoldDate, null, '[]') ||
+      eventEnd.isBetween(startThresoldDate, endThresoldDate, null, '[]')
+    )
+      ||
+    (
+    // case when event contains entirely the thresold.
+      eventStart.isSameOrBefore(startThresoldDate) &&
+      eventEnd.isSameOrAfter(endThresoldDate)
+    )
+  )
+
+type RRuleArgs = ConstructorParameters<typeof RRule> 
+
+// RRule has two arguments: options, noCache
+type RRuleArgsOptions = RRuleArgs[0] 
+type RRuleArgsNoCache = RRuleArgs[1] 
+
+// Our argument extension type: duration
+type Duration = {
+  // TODO: should be always positive
+  amount: moment.DurationInputArg1,
+  unit: moment.unitOfTime.DurationConstructor,
+}
+
+// We extend the original arguments of rrule adding duration
+export type ExtendedRRuleArgsOptions = RRuleArgsOptions & {duration?: Duration}
+
+export default class ExtendedRRule {
+  duration?: Duration;
+  rruleOptions: RRuleArgsOptions;
+  rrule: RRule;
+  constructor(extendedRRuleArgsOptions: ExtendedRRuleArgsOptions, noCache?: RRuleArgsNoCache) {
+    const { duration, ...rruleOptions } = extendedRRuleArgsOptions
+    this.duration = duration
+    this.rruleOptions = rruleOptions
+    this.rrule = new RRule(rruleOptions, noCache)
+  }
+
+  all(...args: Parameters<typeof this.rrule.all>) {
+    return this.rrule.all(...args)
+  }
+
+  // returns the ocurrences that intersects the given range in arguments (considering duration)
+  between(...args: Parameters<typeof this.rrule.between>) {
+
+    // The inc argument (third argument) defines what happens if dt is an occurrence. With inc == true, if dt itself is an occurrence, it will be returned.
+    args[2] = true
+
+    // if no duration, return the result of the original method
+    if (!this.duration) {
+      return this.rrule.between(...args) 
+    }
+
+    // after and before naming is pretty confusing. Rename to inferior and superior limits
+    const [ after, before ] = args
+    const [ inferiorLimit, superiorLimit ] = [ after, before ]
+
+    // we get the closest ocurrence on left to check if the duration intersects the analyzed range
+    const adjancentInferiorDateStart = this.rrule.before(inferiorLimit)
+    // The right ocurrence will never intersect since the duration is walys positive. Ignore.
+    // const adjancentInferiorDate = this.rrule.after(after)
+    // TODO: que pasa si mas de una ocurrencia intersecciona el rango inferior? solo estoy teniendo en cuenta una fecha
+    
+    if (!adjancentInferiorDateStart) {
+      return this.rrule.between(...args) 
+    }
+
+    const adjancentInferiorDateEnd = moment(adjancentInferiorDateStart).add(this.duration.amount, this.duration.unit)
+
+    console.log({adjancentInferiorDateStart: adjancentInferiorDateStart.toISOString()})
+    console.log({adjancentInferiorDateEnd: adjancentInferiorDateEnd.toISOString()})
+    // TODO: review inclusivity
+    // const intersectsTheRange = moment(adjancentInferiorDateEnd).isBetween(inferiorLimit, superiorLimit, null, '[]')
+    const intersectsTheThresold = eventIntersectThresold(
+      moment(adjancentInferiorDateStart),
+      adjancentInferiorDateEnd,
+      moment(inferiorLimit),
+      moment(superiorLimit)
+    )
+
+    if (intersectsTheThresold) {
+      return [adjancentInferiorDateStart.toISOString(), ...this.rrule.between(...args)]
+    }
+
+    return this.rrule.between(...args)
+  }
+  // returns the first ocurrence before the given date in arguments
+  before(...args: Parameters<typeof this.rrule.before>) {
+    return this.rrule.before(...args)
+  }
+  // returns the first ocurrence after the given date in arguments
+  after(...args: Parameters<typeof this.rrule.after>) {
+    return this.rrule.after(...args)
+  }
+
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,16 +1,176 @@
-import { returnArg } from './../src'
+import { datetime, RRule } from 'rrule'
+import ExtendedRRule, { ExtendedRRuleArgsOptions } from './../src'
 
-const myBeverage = {
-  delicious: true,
-  sour: false,
+
+const _ = (obj) => {
+  return JSON.stringify(obj)
 }
 
-describe('my beverage', () => {
-  test('is delicious', () => {
-    expect(returnArg(myBeverage.delicious)).toBeTruthy()
-  })
 
-  test('is not sour', () => {
-    expect(returnArg(myBeverage.sour)).toBeFalsy()
+const RRuleDefinitionA = {
+  freq: RRule.WEEKLY,
+  interval: 5,
+  byweekday: [RRule.MO, RRule.FR],
+  dtstart: datetime(2012, 2, 1, 10, 30),
+  until: datetime(2012, 12, 31),
+}
+
+
+describe('ExtendedRRule behaves similar to RRule while lacking duration', () => {
+  const originalRRule = new RRule(RRuleDefinitionA)
+  const extendedRRule = new ExtendedRRule(RRuleDefinitionA)
+  test('when using "all" method', () => {
+    expect(_(originalRRule.all())).toBe(_(extendedRRule.all()))
+    // console.log('all', originalRRule.all())
+      // ...
+      // 2012-06-22T10:30:00.000Z,
+      // 2012-07-23T10:30:00.000Z,
+      // 2012-07-27T10:30:00.000Z,
+      // 2012-08-27T10:30:00.000Z,
+      // ...
+  })
+  test('when using "between" method', () => {
+    const after = new Date('2012-06-22T10:30:00.000Z')
+    const before = new Date('2012-08-27T10:30:00.000Z')
+    expect(_(originalRRule.between(after, before, true))) // extendedRRule sets 'include' as true by default
+      .toBe(_(extendedRRule.between(after, before)))
+    // console.log('between', originalRRule.between(after, before))
+      // ...
+      // 2012-06-22T10:30:00.000Z, (after this)
+      // 2012-07-23T10:30:00.000Z, -> (return this)
+      // 2012-07-27T10:30:00.000Z, -> (return this)
+      // 2012-08-27T10:30:00.000Z, (before this)
+      // ...
+  })
+  test('when using "before" method', () => {
+    const before = new Date('2012-08-27T10:30:00.000Z')
+    expect(_(originalRRule.before(before)))
+      .toBe(_(extendedRRule.before(before)))
+    // console.log('before', originalRRule.before(before))
+      // ...
+      // 2012-06-22T10:30:00.000Z,
+      // 2012-07-23T10:30:00.000Z,
+      // 2012-07-27T10:30:00.000Z, -> (return this)
+      // 2012-08-27T10:30:00.000Z, (before this)
+      // ...
+  })
+  test('when using "after" method', () => {
+    const after = new Date('2012-06-22T10:30:00.000Z')
+    expect(_(originalRRule.after(after)))
+      .toBe(_(extendedRRule.after(after)))
+    // console.log('after', originalRRule.after(after))
+      // ...
+      // 2012-06-22T10:30:00.000Z, (after this)
+      // 2012-07-23T10:30:00.000Z, -> (return this)
+      // 2012-07-27T10:30:00.000Z,
+      // 2012-08-27T10:30:00.000Z, 
+      // ...
+  })
+})
+
+const RRuleDefinitionB: ExtendedRRuleArgsOptions = {
+  freq: RRule.DAILY,
+  byweekday: [RRule.MO, RRule.FR],
+  dtstart: datetime(2024, 1, 29, 10, 0),
+  duration: {
+    amount: 2,
+    unit: 'hours'
+  }
+}
+
+describe('ExtendedRRule works correctly with a duration of 2 hours', () => {
+  const extendedRRule = new ExtendedRRule(RRuleDefinitionB)
+  test('when first limit of range intersects the duration of first ocurrence', () => {
+    const after = new Date('2024-01-29T11:00:00.000Z')
+    const before = new Date('2024-02-02T11:00:00.000Z')
+    expect(_(extendedRRule.between(after, before)))
+      .toBe(_(['2024-01-29T10:00:00.000Z', '2024-02-02T10:00:00.000Z']))
+    // [
+    //   '2024-01-29T10:00:00.000Z', -> (return this)
+    //                               (range intersects the duration of the first element)
+    //   '2024-02-02T10:00:00.000Z', -> (return this)
+    //   '2024-02-05T10:00:00.000Z',
+    //   '2024-02-09T10:00:00.000Z',
+    //   '2024-02-12T10:00:00.000Z',
+    //   '2024-02-16T10:00:00.000Z',
+    //   ...
+  })
+  test('when first limit of range intersects the duration of second ocurrence', () => {
+    const after = new Date('2024-02-02T09:00:00.000Z')
+    const before = new Date('2024-02-02T11:00:00.000Z')
+    expect(_(extendedRRule.between(after, before)))
+      .toBe(_(['2024-02-02T10:00:00.000Z']))
+    // [
+    //   '2024-01-29T10:00:00.000Z', -> (return this)
+    //                               (range intersects the duration of the first element)
+    //   '2024-02-02T10:00:00.000Z',
+    //   '2024-02-05T10:00:00.000Z',
+    //   '2024-02-09T10:00:00.000Z',
+    //   '2024-02-12T10:00:00.000Z',
+    //   '2024-02-16T10:00:00.000Z',
+    //   ...
+  })
+  test('when limits of range match exact ocurrences', () => {
+    const after = new Date('2024-02-02T10:00:00.000Z')
+    const before = new Date('2024-02-09T10:00:00.000Z')
+    expect(_(extendedRRule.between(after, before)))
+      .toBe(_(['2024-02-02T10:00:00.000Z', '2024-02-05T10:00:00.000Z', '2024-02-09T10:00:00.000Z']))
+    // [
+    //   '2024-01-29T10:00:00.000Z',
+    //   '2024-02-02T10:00:00.000Z', (after) -> (return this)
+    //   '2024-02-05T10:00:00.000Z',         -> (return this)
+    //   '2024-02-09T10:00:00.000Z', (before)-> (return this)
+    //   '2024-02-12T10:00:00.000Z',
+    //   '2024-02-16T10:00:00.000Z',
+    //   ...
+  })
+})
+
+const RRuleDefinitionC: ExtendedRRuleArgsOptions = {
+  freq: RRule.MONTHLY,
+  dtstart: datetime(2024, 1, 1, 10, 0),
+  duration: {
+    amount: 15,
+    unit: 'days'
+  }
+}
+
+describe('ExtendedRRule works correctly with a duration of 15 days', () => {
+  const extendedRRule = new ExtendedRRule(RRuleDefinitionC)
+  test('when limit of range (of 1 day) intersects the duration of first ocurrence', () => {
+    const after = new Date('2024-02-05T11:00:00.000Z')
+    const before = new Date('2024-02-06T11:00:00.000Z')
+    expect(_(extendedRRule.between(after, before)))
+      .toBe(_(['2024-02-01T10:00:00.000Z']))
+    // [
+    //   '2024-01-01T10:00:00.000Z',
+    //   '2024-02-01T10:00:00.000Z', -> (return this)
+    //                               (range intersects the duration of the previous element, but not the event itself)
+    //   '2024-03-01T10:00:00.000Z',
+    //   ...
+  })
+  test('when limit of range (of 2 hours) intersects the duration on superior limit', () => {
+    const after = new Date('2024-02-16T09:00:00.000Z')
+    const before = new Date('2024-02-16T11:00:00.000Z')
+    expect(_(extendedRRule.between(after, before)))
+      .toBe(_(['2024-02-01T10:00:00.000Z']))
+    // [
+    //   '2024-01-01T10:00:00.000Z',
+    //   '2024-02-01T10:00:00.000Z', -> (return this)
+    //                               (range intersects the duration of the previous element, but not the event itself)
+    //   '2024-03-01T10:00:00.000Z',
+    //   ...
+  })
+  test('when limit of range (of 5 days) doesnt intersect the duration', () => {
+    const after = new Date('2024-02-20T09:00:00.000Z')
+    const before = new Date('2024-02-25T11:00:00.000Z')
+    expect(_(extendedRRule.between(after, before)))
+      .toBe(_([]))
+    // [
+    //   '2024-01-01T10:00:00.000Z',
+    //   '2024-02-01T10:00:00.000Z', -> (return this)
+    //                               (range intersects the duration of the previous element, but not the event itself)
+    //   '2024-03-01T10:00:00.000Z',
+    //   ...
   })
 })
